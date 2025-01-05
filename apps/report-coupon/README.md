7cf6c9d78fc5368ba281500e8a5c5e3a71eb9887
Jason Kuo <jason.kuo41@hotmail.com.tw>
2025-01-05 11:11:02 -0500

    Add script to initialize coupon database

---
+++ apps/report-coupon/README.md
@@ -0,0 +1,4 @@
+To initialize the database type this in the terminal
+```
+npx wrangler d1 execute slime-coupon --local --file=./src/init.sql
+```
---
+++ apps/report-coupon/src/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS CouponTable (
+    Id TEXT PRIMARY KEY,       -- 唯一標識符
+    Domain TEXT NOT NULL,      -- 儲存網站域名
+    UrlRegex TEXT,             -- 儲存爬取折扣碼的正則表達式
+    Coupon TEXT NOT NULL,      -- 折扣碼
+    CouponContent TEXT,        -- 優惠內容描述
+    Conditions TEXT,           -- 優惠使用條件
+    DiscoveredAt NUMBER NOT NULL, -- 什麼時候發現的
+    ExpireAt NUMBER           -- 什麼時候過期
+);
+
+CREATE INDEX IF NOT EXISTS CouponTable_Domain ON CouponTable(Domain)
+
+-- TODO: 之後加一個Table 可以讓人按讚或倒讚
