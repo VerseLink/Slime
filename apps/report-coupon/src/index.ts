34b87888f431ae551fb7ca8b86c5fae8a98ffa74
Jason Kuo <jason.kuo41@hotmail.com.tw>
2025-01-14 07:13:02 -0500

    update auto router

--- apps/report-coupon/src/index.ts
+++ apps/report-coupon/src/index.ts
@@ -11,17 +11,62 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { AutoRouter, error, IRequest, json } from "itty-router";
+import { AutoRouter, error, IRequest, json, StatusError } from "itty-router";
 import { ApiRouter } from "./api/v1";
+import { ApiQueryFailedResponse } from "@slime/api-v1";
 
-const router = AutoRouter<IRequest, [Env, ExecutionContext]>({ base: "/api/v1" });
+const getMessage = (code: number): string => ({
+	400: 'Bad Request',
+	401: 'Unauthorized',
+	403: 'Forbidden',
+	404: 'Not Found',
+	500: 'Internal Server Error',
+})[code] || 'Unknown Error';
 
-router.all("/activity", ApiRouter.activity.fetch)
-	  .all("/stores", ApiRouter.stores.fetch)
-	  .all("/report", ApiRouter.report.fetch)
+const apiError = (err: unknown) => {
+	if (err instanceof StatusError) {
+		const fail: ApiQueryFailedResponse = {
+			success: false,
+			error: err.message,
+		};
+		return json(fail, { status: err.status });
+	}
+
+	let errStat: number;
+	if (typeof err !== "number") {
+		// unexpected error!
+		console.error(err);
+		errStat = 500;
+	}
+	else {
+		errStat = err;
+	}
+	const fail: ApiQueryFailedResponse = {
+		success: false,
+		error: getMessage(errStat),
+	};
+	return json(fail, { status: errStat });
+}
+
+const router = AutoRouter<IRequest, [Env, ExecutionContext]>({
+	base: "/api/v1",
+	catch: apiError,
+	/**
+	format: (response) => {
+		return json({
+			success: true,
+			result: response,
+		});
+	},
+	*/
+	missing: () => apiError(404)
+})
+	.all("/activity", ApiRouter.activity.fetch)
+	.all("/stores", ApiRouter.stores.fetch)
+	.all("/report", ApiRouter.report.fetch);
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return router.fetch(request, env, ctx).then(json).catch(error);
+		return await router.fetch(request, env, ctx);
 	}
 } satisfies ExportedHandler<Env>;
